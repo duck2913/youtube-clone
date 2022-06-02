3302e77a5e5e280a1cb8b93049dc2e1583ba5698
duck2913 <minhducdl1010@gmail.com>
2022-06-02 16:42:53 +0700

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Grid } from "@mui/material";
+import youtube from "./api/youtube";
+import SearchBar from "./components/SearchBar";
+import VideoList from "./components/VideoList";
+import VideoDetail from "./components/VideoDetail";
+
 function App() {
-	return <></>;
+	return (
+		<Grid container spacing={2} justifyContent="center">
+			<Grid item xs={12}>
+				<SearchBar />
+			</Grid>
+			<Grid item xs={8}>
+				<VideoDetail />
+			</Grid>
+			<Grid item xs={4}>
+				<VideoList />
+			</Grid>
+		</Grid>
+	);
 }
 
 export default App;
